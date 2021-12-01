69b24ef4ac2c7eda76feac63ff5b44af8d89cd68
Will Tuffin <will.tuffin@raytheon.co.uk>
2021-12-01 16:32:19 +0000

    remove demo pipeline

--- createJobs.groovy
+++ createJobs.groovy
@@ -1,12 +1,3 @@
-pipelineJob('pipelineJob') {
-    definition {
-        cps {
-            script(readFileFromWorkspace('pipelineJob.groovy'))
-            sandbox()
-        }
-    }
-}
-
 pipelineJob('spring-boot-demo-job') {
     definition {
         cpsScm {
